bc4e29a85ad284950efa8eef2dac65aa097c75e3
AreyBRSK <areybrsk@gmail.com>
2022-03-28 10:29:57 +0300

    Initial Commit

---
+++ B2L4_PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B2L4_PersonalityQuiz
+//
+//  Created by Андрей Барсук on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
